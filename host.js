1fac0687f2aa594a8142b0f1e2f351aaa775b0fc
Aspect <griffin.siebert@stu.jefferson.kyschools.us>
2022-02-17 13:12:38 -0500

    ui ooohhhh spookyyyy

---
+++ host.js
@@ -0,0 +1,18 @@
+/*
+
+// AUTOCON Server "Virus" Infected Host file
+© IAN Studios 2021, All rights unreserved.
+
+This code is free. take it.
+
+For use in AUTOCON pacakge. Edits to file may cause corruption or large errors.
+
+IDK why i wrote all of that
+
+
+// THIS FILE IS IDENTICAL BECAUSE THIS FILE IS THE ONE PUT ON THE SERVERS
+
+*/
+
+export async function main(ns) {
+}
--- main.js
+++ main.js
@@ -9,37 +9,80 @@ For use in AUTOCON pacakge. Edits to file may cause corruption or large errors.
 
 IDK why i wrote all of that
 
-How the code works:
-
-Step 1: Scan all servers at current depth (TODO: Remove owned server from list for convinence)
-Step 2: Test every server to see if it is viable for attack
-Step 2.1: If server can be nuked, or made viable for attack, run nuke or hack system
-Step 3: Upload all files requried for efficient attack
-Step 4: Run scripts
-Step 5: ???
-Step 6: Profit
-
-The goal of true efficiency can be made by simply lowering the security of the server- Once low enough,
-Hacking and weakening will take less and less.
-
-Theoretically.
-
 */
 
 // Control System
 export async function main(ns) {
 
 	// Await user confirmation
-	var usr_confirmed = await ns.prompt("Would you like to run the AUTOCON server virus?");
+	var usr_confirmed = await ns.prompt("Would you like to run the AUTOCON server virus? WARNING: SERVERS YOU OWN WILL HAVE FILES ADDED TO THEM. TO PREVENT THIS, ADD THEIR HOSTNAMES IN THE [srv_blacklist] ARRAY IN THE /AUTOCON/main.js FILE!");
 	if (usr_confirmed == false) {
-		await ns.prompt("Cancelling run of AUTOCON");
+		ns.toast("AUTOCON Main System Run Cancelled","error")
 		return;
 	}
 
-	//Get valid list of all servers and narrow them down to the ones that are hackable.
-	await connect("home"); // Ensures you are at root
 
+
+
+	// Var used for Blacklisting Servers
+	// E.G. ["Home","darkweb","serverhostname1","serverhostname2"]
+	//
+	var srv_blacklist = ["home","darkweb"];
+	//
+	//
+
+
+
+
+
+	var i = 0;
 	var srvlst = new Array()
 	srvlst = await ns.scan();
-	
+    
+
+	// Information directed towards user.
+	// Pro tip: 250 ms in sleep time is almost perfect for preventing the main toast from being flooded out.
+	//
+    ns.toast("Releasing AUTOCON Virus.","warning",10000);
+
+    while (i!=srvlst.length) {
+		var srv = srvlst[i];
+        ns.toast(`Scan Discovered Server ${srv}`,"info")
+		await ns.sleep(250);
+
+		if (srv_blacklist.includes(srv)) {
+			ns.toast(`Server ${srv} Blocked from being infected by Blacklist`,"success")
+			await ns.sleep(250);
+		} else {
+
+			// Logic Breakdown (ooh spooky);
+			//
+			// Phase 1: Checks
+			//	- Check if server has AUTOCON pre-installed. If so, cancel.
+			//	- Check if player meets minimum requirement for running code on server. If not, cancel, and add to event log.
+			//
+			//	- The Special Check - Check depth level of current server compared to the main depth counter, so if bot gets stuck, we push it back up.
+			//
+			//
+			// Phase 2: The fun part
+			//	- Uplaod all viral information to server
+			//	- Run Phase 1 one final time to ensure success; if failed, print an error.
+			//	- Upload Control File (./host.js)
+			//
+			// Phase 3: Profit
+			//	- Triple check that all servers available are fully operational
+			//	- Run each Server Control file to run scripts
+			//	- Run each Server Control file to spread Virus deeper and deeper. 
+			//	- Await message from each Control File to return, ending the script and releasing lots of debug info
+
+			
+
+
+
+		}
+		
+		// End Cycle
+        i=i+1;
+    }
+
 }
