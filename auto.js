0aed1de3a7a24b82731c35d23692f13584bea82a
Aspect <griffin.siebert@stu.jefferson.kyschools.us>
2022-02-18 13:13:42 -0500

    h

---
+++ auto.js
@@ -0,0 +1,10 @@
+export async function addControlPanel() {
+    let statspanel = document.getElementsByClassName("css-oa3chk");
+    let ctrlbutton = document.getelem
+    statspanel[0].
+
+}
+
+/** @param {NS} ns **/
+export async function main(ns) {
+}
